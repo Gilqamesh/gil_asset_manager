2026714b98ccd69433c96f82d2dcdcb924ad52e1
gilqamesh <celicaw8@gmail.com>
2024-06-22 21:16:02 +0200

    Added: asset manager - default ctor, setter for initial values

--- asset_manager.cpp
+++ asset_manager.cpp
@@ -12,3 +12,11 @@ asset_manager_t::~asset_manager_t() {
     }
     m_asset_type_to_asset_loader.clear();
 }
+
+
+void asset_manager_t::init(const std::string& server_scheme, const std::string& server_host_name, size_t server_port) {
+    m_server_scheme = server_scheme;
+    m_server_host_name = server_host_name;
+    m_server_port = server_port;
+}
+
